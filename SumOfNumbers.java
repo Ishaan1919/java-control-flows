17aba10705e772634a073601d1087f103e21034f
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-03 15:29:50 +0530

    Program to find the sum of n natural numbers [2nd commit]

--- SumOfNumbers.java
+++ SumOfNumbers.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class SumOfNumbers{
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number: ");
 		int number = input.nextInt();
 		if(number<=0){
             System.out.print(number + " is not a natural number");
