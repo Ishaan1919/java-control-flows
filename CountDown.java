0edcbb2e429ac18fa62b879392ca0ecbceb7a924
Ishaan <ishaan0642.be21@chitkara.edu.in>
2025-03-03 19:13:40 +0530

    Program for countdown

---
+++ CountDown.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CountDown {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the countdown start value: ");
+        int counter = input.nextInt();
+        
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--;
+        }
+        
+        System.out.println("Liftoff!");
+        input.close();
+    }
+}
